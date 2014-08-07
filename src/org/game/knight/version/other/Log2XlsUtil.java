594c391663b3499ebc619832ec16078b56e363c1
chw <chw-chengwei@foxmail.com>
2014-08-07 15:27:25 +0800

    error2xsl

--- src/org/game/knight/version/other/Log2XlsUtil.java
+++ src/org/game/knight/version/other/Log2XlsUtil.java
@@ -6,6 +6,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
@@ -92,6 +94,37 @@ public class Log2XlsUtil extends Composite
 
 		logBrowse = new Button(grplog, SWT.NONE);
 		logBrowse.setText("  ...  ");
+
+		errLabel = new Link(grplog, SWT.NONE);
+		errLabel.addSelectionListener(new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				CmdUtil.openDir(errInput.getText());
+			}
+		});
+		errLabel.setText("<a>\u9519\u8BEF\u65E5\u5FD7</a>:");
+
+		errInput = new Text(grplog, SWT.BORDER);
+		errInput.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		errBrowse = new Button(grplog, SWT.NONE);
+		errBrowse.addSelectionListener(new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				DirectoryDialog dir = new DirectoryDialog(getShell());
+				String path = dir.open();
+
+				if (path != null)
+				{
+					errInput.setText(path);
+				}
+			}
+		});
+		errBrowse.setText("  ...  ");
 		new Label(grplog, SWT.NONE);
 
 		submit = new Button(grplog, SWT.NONE);
@@ -281,20 +314,20 @@ public class Log2XlsUtil extends Composite
 	 */
 	private boolean checkTo()
 	{
-//		String to = saveInput.getText();
-//
-//		if (to == null || to == "")
-//		{
-//			return false;
-//		}
-//		else
-//		{
-//			File toFile = new File(to);
-//			if (toFile.exists() && toFile.isDirectory())
-//			{
-//				return false;
-//			}
-//		}
+		// String to = saveInput.getText();
+		//
+		// if (to == null || to == "")
+		// {
+		// return false;
+		// }
+		// else
+		// {
+		// File toFile = new File(to);
+		// if (toFile.exists() && toFile.isDirectory())
+		// {
+		// return false;
+		// }
+		// }
 
 		return true;
 	}
@@ -360,6 +393,119 @@ public class Log2XlsUtil extends Composite
 	private Thread thread;
 	private Link logLabel;
 	private Group grplog;
+	private Link errLabel;
+	private Text errInput;
+	private Button errBrowse;
+
+	
+	/**
+	 * 错误LOG转换到
+	 * @author ds
+	 *
+	 */
+	private class ErrorLog2EXCELTask
+	{
+		private File[] sqlFiles;
+
+		private int index;
+		private int finishCount;
+		private boolean cancel;
+
+		public ErrorLog2EXCELTask(File folder)
+		{
+			ArrayList<File> sqls = new ArrayList<File>();
+
+			File[] files = folder.isDirectory() ? folder.listFiles() : new File[] {};
+			for (File file : files)
+			{
+				if (file.isFile() && !file.isHidden() && file.getPath().endsWith(".sql"))
+				{
+					File xsl = new File(file.getPath().replaceAll("\\.sql$", ".xsl"));
+					if (!xsl.exists())
+					{
+						sqls.add(file);
+					}
+				}
+			}
+
+			sqlFiles = sqls.toArray(new File[sqls.size()]);
+		}
+
+		public void start()
+		{
+			ExecutorService exec = Executors.newFixedThreadPool(5);
+			for (int i = 0; i < 5; i++)
+			{
+				exec.execute(new ErrorRunnable());
+			}
+		}
+
+		public synchronized void cancel()
+		{
+			cancel = true;
+		}
+		
+		public synchronized int getFinishCount()
+		{
+			return finishCount;
+		}
+		
+		public synchronized int getTotalCount()
+		{
+			return sqlFiles.length;
+		}
+
+		private synchronized File getNextFile()
+		{
+			File result = null;
+			if (index < sqlFiles.length)
+			{
+				result = sqlFiles[index];
+				index++;
+			}
+			return result;
+		}
+
+		private synchronized boolean isCancel()
+		{
+			return cancel;
+		}
+
+		private class ErrorRunnable implements Runnable
+		{
+			@Override
+			public void run()
+			{
+				while(true)
+				{
+					File file=getNextFile();
+					if(file==null || isCancel())
+					{
+						break;
+					}
+					
+					writeXls(file);
+				}
+			}
+			
+			private void writeXls(File file)
+			{
+				try
+				{
+					String text = new String(FileUtil.getFileBytes(file), "utf8");
+					String[] tables=text.split("\\nBye\\n");
+					for(String table:tables)
+					{
+						table=table.replaceAll("--------------\\n(.*?)\\n--------------\\n\\n", "$1\n");
+					}
+				}
+				catch (UnsupportedEncodingException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 	/**
 	 * 运行
@@ -367,7 +513,7 @@ public class Log2XlsUtil extends Composite
 	private void run()
 	{
 		final String fromURL = logInput.getText();
-//		final String destPath = saveInput.getText();
+		final String errorURL = errInput.getText();
 
 		thread = new Thread(new Runnable()
 		{
@@ -421,7 +567,7 @@ public class Log2XlsUtil extends Composite
 						}
 					});
 				}
-				
+
 				boolean error = false;
 
 				ArrayList<File> sqls = new ArrayList<File>();
@@ -429,23 +575,20 @@ public class Log2XlsUtil extends Composite
 				File logDir = new File(fromURL);
 				for (File logFile : logDir.listFiles())
 				{
-					if (logFile.isHidden())
+					if (logFile.isFile() && !logFile.isHidden() && logFile.getName().toLowerCase().endsWith(".sql"))
 					{
-						continue;
-					}
-					if (!logFile.isFile())
-					{
-						continue;
-					}
-					if (!logFile.getName().toLowerCase().endsWith(".sql"))
-					{
-						continue;
-					}
+						String url = logFile.getPath();
+						url = url.substring(0, url.length() - ".sql".length()) + ".xls";
 
-					sqls.add(logFile);
+						File tmp = new File(url);
+						if (!tmp.exists())
+						{
+							sqls.add(logFile);
+						}
+					}
 				}
 
-				final int sqlCount=sqls.size();
+				final int sqlCount = sqls.size();
 				if (!isDisposed())
 				{
 					getDisplay().syncExec(new Runnable()
@@ -458,14 +601,14 @@ public class Log2XlsUtil extends Composite
 						}
 					});
 				}
-				
-				for (int i=0;i<sqls.size();i++)
-				{
-					File file=sqls.get(i);
 
-					HSSFWorkbook book=new HSSFWorkbook();
-					
-					final int sqlI=i+1;
+				for (int i = 0; i < sqls.size(); i++)
+				{
+					File file = sqls.get(i);
+
+					HSSFWorkbook book = new HSSFWorkbook();
+
+					final int sqlI = i + 1;
 					if (!isDisposed())
 					{
 						getDisplay().syncExec(new Runnable()
@@ -477,34 +620,34 @@ public class Log2XlsUtil extends Composite
 							}
 						});
 					}
-					
-					ArrayList<String> counts=new ArrayList<String>();
-					ArrayList<String> comms=new ArrayList<String>();
-					
+
+					ArrayList<String> counts = new ArrayList<String>();
+					ArrayList<String> comms = new ArrayList<String>();
+
 					try
 					{
 						String text = new String(FileUtil.getFileBytes(file), "utf8");
 						text = text.replaceAll(" *(.*?)\\s*\\n--------------\\n(select count.*?)\\n--------------\\s*\\+.*?\\+\\s*\\|\\s*count.*?\\n\\s*\\+.*?\\+\\s\\|                     (\\s*[0-9]*)\\s\\|\\s*\\+.*?\\+\\s.*?\\n\\s*Bye.*?\\n(\\s*剧情.*?\\n)?", "$3\t$1\n");
-						String[] lines=text.split("\\n");
-						for(String line:lines)
+						String[] lines = text.split("\\n");
+						for (String line : lines)
 						{
-							line=line.trim();
-							int index=line.indexOf("\t");
-							if(index!=-1)
+							line = line.trim();
+							int index = line.indexOf("\t");
+							if (index != -1)
 							{
-								String part1=line.substring(0, index).trim();
-								String part2=line.substring(index+1).trim();
-								
-								int count=-1;
+								String part1 = line.substring(0, index).trim();
+								String part2 = line.substring(index + 1).trim();
+
+								int count = -1;
 								try
 								{
-									count=Integer.parseInt(part1);
+									count = Integer.parseInt(part1);
 								}
-								catch(Error err)
+								catch (Error err)
 								{
 								}
-								
-								if(count!=-1 && !part2.equals("起名出错"))
+
+								if (count != -1 && !part2.equals("起名出错"))
 								{
 									counts.add(part1);
 									comms.add(part2);
@@ -516,176 +659,176 @@ public class Log2XlsUtil extends Composite
 					{
 						e.printStackTrace();
 						showError(e.getMessage());
-						error=true;
+						error = true;
 					}
-					
-					HSSFCellStyle countStyle=book.createCellStyle();
+
+					HSSFCellStyle countStyle = book.createCellStyle();
 					countStyle.setAlignment(HSSFCellStyle.ALIGN_RIGHT);
 					countStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
-					
-					HSSFCellStyle commStyle=book.createCellStyle();
+
+					HSSFCellStyle commStyle = book.createCellStyle();
 					commStyle.setAlignment(HSSFCellStyle.ALIGN_LEFT);
 					commStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
 
-					HSSFCellStyle headStyle=book.createCellStyle();
+					HSSFCellStyle headStyle = book.createCellStyle();
 					headStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
 					headStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
 					headStyle.setFillForegroundColor(HSSFColor.TEAL.index);
-					HSSFFont font=book.createFont();
+					HSSFFont font = book.createFont();
 					font.setColor(HSSFColor.WHITE.index);
-					font.setBoldweight((short)2);
+					font.setBoldweight((short) 2);
 					headStyle.setFont(font);
 					headStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-					
-					HSSFCellStyle numberStyle=book.createCellStyle();
+
+					HSSFCellStyle numberStyle = book.createCellStyle();
 					numberStyle.setAlignment(HSSFCellStyle.ALIGN_RIGHT);
 					numberStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
 					numberStyle.setDataFormat(HSSFDataFormat.getBuiltinFormat("0.00%"));
-					
-					int prev1=-1;
-					
-					HSSFSheet sheet=book.createSheet(file.getName());
+
+					int prev1 = -1;
+
+					HSSFSheet sheet = book.createSheet(file.getName());
 					sheet.setColumnWidth(0, 10000);
 					sheet.setColumnWidth(1, 3000);
 					sheet.setColumnWidth(2, 3000);
 					sheet.setColumnWidth(3, 3000);
 					sheet.setColumnWidth(4, 3000);
 					sheet.setColumnWidth(6, 5000);
-					
-					HSSFRow title=sheet.createRow(0);
-					title.setHeight((short)500);
-					HSSFCell head=null;
-					head=title.createCell(0);
+
+					HSSFRow title = sheet.createRow(0);
+					title.setHeight((short) 500);
+					HSSFCell head = null;
+					head = title.createCell(0);
 					head.setCellValue("说明");
 					head.setCellStyle(headStyle);
 					head.setCellType(HSSFCell.CELL_TYPE_STRING);
-					head=title.createCell(1);
+					head = title.createCell(1);
 					head.setCellValue("人数");
 					head.setCellStyle(headStyle);
 					head.setCellType(HSSFCell.CELL_TYPE_STRING);
-					head=title.createCell(2);
+					head = title.createCell(2);
 					head.setCellValue("流存比");
 					head.setCellStyle(headStyle);
 					head.setCellType(HSSFCell.CELL_TYPE_STRING);
-					head=title.createCell(3);
+					head = title.createCell(3);
 					head.setCellValue("流失比");
 					head.setCellStyle(headStyle);
 					head.setCellType(HSSFCell.CELL_TYPE_STRING);
-					head=title.createCell(4);
+					head = title.createCell(4);
 					head.setCellValue("流失数");
 					head.setCellStyle(headStyle);
 					head.setCellType(HSSFCell.CELL_TYPE_STRING);
-					
-					int index=1;
-					for(int j=0;j<counts.size();j++)
+
+					int index = 1;
+					for (int j = 0; j < counts.size(); j++)
 					{
-						int curr=Integer.parseInt(counts.get(j));
-						if(curr==0)
+						int curr = Integer.parseInt(counts.get(j));
+						if (curr == 0)
 						{
 							continue;
 						}
-						
-						HSSFRow row=sheet.createRow(index);
-						
-						HSSFCell cell=null;
-						
-						cell=row.createCell(0);
+
+						HSSFRow row = sheet.createRow(index);
+
+						HSSFCell cell = null;
+
+						cell = row.createCell(0);
 						cell.setCellValue(comms.get(j));
 						cell.setCellStyle(commStyle);
 						cell.setCellType(HSSFCell.CELL_TYPE_STRING);
-						
-						cell=row.createCell(1);
+
+						cell = row.createCell(1);
 						cell.setCellValue(curr);
 						cell.setCellStyle(countStyle);
 						cell.setCellType(HSSFCell.CELL_TYPE_NUMERIC);
-						
-						if(index>1 && curr>0)
+
+						if (index > 1 && curr > 0)
 						{
-							cell=row.createCell(2);
-							cell.setCellValue((double)curr/prev1);
+							cell = row.createCell(2);
+							cell.setCellValue((double) curr / prev1);
 							cell.setCellStyle(numberStyle);
 							cell.setCellType(HSSFCell.CELL_TYPE_NUMERIC);
 
-							cell=row.createCell(3);
-							cell.setCellValue(1-((double)curr)/prev1);
+							cell = row.createCell(3);
+							cell.setCellValue(1 - ((double) curr) / prev1);
 							cell.setCellStyle(numberStyle);
 							cell.setCellType(HSSFCell.CELL_TYPE_NUMERIC);
 
-							cell=row.createCell(4);
-							cell.setCellValue(prev1-curr);
+							cell = row.createCell(4);
+							cell.setCellValue(prev1 - curr);
 							cell.setCellStyle(countStyle);
 							cell.setCellType(HSSFCell.CELL_TYPE_NUMERIC);
-							
-							if(index>2)
+
+							if (index > 2)
 							{
 								HSSFSheetConditionalFormatting scf = sheet.getSheetConditionalFormatting();
-								
-								HSSFConditionalFormattingRule rule1 = scf.createConditionalFormattingRule("C"+index+"<0.99001");
+
+								HSSFConditionalFormattingRule rule1 = scf.createConditionalFormattingRule("C" + index + "<0.99001");
 								HSSFPatternFormatting cf1 = rule1.createPatternFormatting();
 								cf1.setFillBackgroundColor(HSSFColor.LIGHT_GREEN.index);
-								
-								HSSFConditionalFormattingRule rule2 = scf.createConditionalFormattingRule("C"+index+"<0.98001");
+
+								HSSFConditionalFormattingRule rule2 = scf.createConditionalFormattingRule("C" + index + "<0.98001");
 								HSSFPatternFormatting cf2 = rule2.createPatternFormatting();
 								cf2.setFillBackgroundColor(HSSFColor.LIGHT_YELLOW.index);
-								
-								HSSFConditionalFormattingRule rule3 = scf.createConditionalFormattingRule("C"+index+"<0.97001");
+
+								HSSFConditionalFormattingRule rule3 = scf.createConditionalFormattingRule("C" + index + "<0.97001");
 								HSSFPatternFormatting cf3 = rule3.createPatternFormatting();
 								cf3.setFillBackgroundColor(HSSFColor.LIGHT_ORANGE.index);
-								scf.addConditionalFormatting(new CellRangeAddress[] { CellRangeAddress.valueOf("A" + index+ ":E" + index + "") }, new HSSFConditionalFormattingRule[] { rule3,rule2,rule1 });
+								scf.addConditionalFormatting(new CellRangeAddress[] { CellRangeAddress.valueOf("A" + index + ":E" + index + "") }, new HSSFConditionalFormattingRule[] { rule3, rule2, rule1 });
 							}
-							
-							if(index==5)
+
+							if (index == 5)
 							{
-								HSSFCellStyle cellStyle=book.createCellStyle();
+								HSSFCellStyle cellStyle = book.createCellStyle();
 								cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
 								cellStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
 								cellStyle.setFillForegroundColor(HSSFColor.LIGHT_GREEN.index);
 								cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-								
-								cell=row.createCell(6);
+
+								cell = row.createCell(6);
 								cell.setCellValue("流存比低于99%");
 								cell.setCellStyle(cellStyle);
 								cell.setCellType(HSSFCell.CELL_TYPE_STRING);
 							}
-							else if(index==6)
+							else if (index == 6)
 							{
-								HSSFCellStyle cellStyle=book.createCellStyle();
+								HSSFCellStyle cellStyle = book.createCellStyle();
 								cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
 								cellStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
 								cellStyle.setFillForegroundColor(HSSFColor.LIGHT_YELLOW.index);
 								cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-								
-								cell=row.createCell(6);
+
+								cell = row.createCell(6);
 								cell.setCellValue("流存比低于98%");
 								cell.setCellStyle(cellStyle);
 								cell.setCellType(HSSFCell.CELL_TYPE_STRING);
 							}
-							else if(index==7)
+							else if (index == 7)
 							{
-								HSSFCellStyle cellStyle=book.createCellStyle();
+								HSSFCellStyle cellStyle = book.createCellStyle();
 								cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
 								cellStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
 								cellStyle.setFillForegroundColor(HSSFColor.LIGHT_ORANGE.index);
 								cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-								
-								cell=row.createCell(6);
+
+								cell = row.createCell(6);
 								cell.setCellValue("流存比低于97%");
 								cell.setCellStyle(cellStyle);
 								cell.setCellType(HSSFCell.CELL_TYPE_STRING);
 							}
 						}
-						
-						if(curr>0)
+
+						if (curr > 0)
 						{
-							prev1=curr;
+							prev1 = curr;
 						}
-						
+
 						index++;
 					}
-					
+
 					try
 					{
-						FileOutputStream output = new FileOutputStream(new File(file.getPath()+".xls"));
+						FileOutputStream output = new FileOutputStream(new File(file.getPath() + ".xls"));
 						book.write(output);
 						output.close();
 					}
@@ -693,18 +836,18 @@ public class Log2XlsUtil extends Composite
 					{
 						e.printStackTrace();
 						showError(e.getMessage());
-						error=true;
+						error = true;
 						break;
 					}
 					catch (IOException e)
 					{
 						e.printStackTrace();
 						showError(e.getMessage());
-						error=true;
+						error = true;
 						break;
 					}
-					
-					if(stoped)
+
+					if (stoped)
 					{
 						break;
 					}
